4bd1acaaeb8cbd64adf6ce11ef81fbedab444b06
Danil Tkachev <cymrac2013@yandex.ru>
2021-10-05 18:15:17 +0300

    serializer cleaned

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -15,7 +15,6 @@ class Statistics_serializer(serializers.ModelSerializer):
         """
         min_date = date(2000, 1, 1)
         max_date = date.today()
-        print(max_date)
         if date_ <= min_date or date_ >= max_date:
             raise serializers.ValidationError('The date has to be >= 2000-01-01 and <= today')
         else:
@@ -32,7 +31,6 @@ class Statistics_serializer(serializers.ModelSerializer):
         """
         self.validated_data['cpc'] = calc_cpc(self.validated_data)
         self.validated_data['cpm'] = calc_cpm(self.validated_data)
-        print(self.validated_data)
         return super(Statistics_serializer, self).save()
 
     class Meta:
